f52ea8dd1deeca52a5df1daec8f0a6d794cbf1b4
nastyatext <129894214+nastyatext@users.noreply.github.com>
2023-04-21 23:10:19 -0700

    Add files via upload

---
+++ week2/src/MyVariable1.java
@@ -0,0 +1,28 @@
+
+public class MyVariable1 {
+
+    public static void main(String[] args) {
+        String name = "Anastasiya";
+        int hours = 40;
+        System.out.println(name);
+        System.out.println(hours);
+
+//       String building, tree, lace, ocean;
+//        tree ="Redwood";
+//        building = "Empire State";
+//        lace = "Lace Superior";
+//        ocean = "Pacific";
+
+        String building = "Empire State", tree ="Redwood", lace = "Lace Superior", ocean = "Pacific";
+        int weeks = 4, months = 5, years = 10, decades = 20;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lace);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(months);
+        System.out.println(years);
+        System.out.println(decades);
+    }
+}
